f7af583b91667c023181c737ea733fdd7b92560c
Raman <Ramandeepkaur16@acedemic.com>
2024-11-05 01:38:39 -0600

    Added NASA API fetch function and integrated with button click

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ window.addEventListener("load", function() {
         }
     });
 });
+const API_KEY = 'h08s2AsUTou9SVmFT9A6EyV0OnWUQSBF25uSorDy';
+
+function fetchPhotos(date) {
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${API_KEY}`;
+
+    console.log("Fetching photos from NASA API");
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Photos received from API:", data);
+        })
+        .catch(error => {
+            console.log("Error fetching photos:", error);
+        });
+}
+loadPhotosButton.addEventListener('click', function() {
+    const selectedDate = dateInput.value;
+    if (selectedDate) {
+        console.log("Date selected:", selectedDate);
+        fetchPhotos(selectedDate);
+    } else {
+        alert('Please select a date first.');
+        console.log("No date selected");
+    }
+});
+
